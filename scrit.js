95e1a81eddea4bac664da9e3bf8b08c189710113
EmmA <scanixemmaakwilani3@gmail.com>
2022-02-18 14:24:13 +0300

    first commit

---
+++ scrit.js
@@ -0,0 +1,12 @@
+const registerbtn = document.querySelector('.input-group-register')
+const btnRegister = document.querySelector('.btn-register')
+
+
+btnRegister.addEventListener("click", Register)
+
+function Register()
+{
+    registerbtn.style.display = "block"'
+    
+}
+
